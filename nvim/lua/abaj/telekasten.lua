fcbc6ca06fb21b4facb7b6ca1eaeec4faf302a66
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-27 11:39:00 +1000

    cron 11:39:00

---
+++ nvim/lua/abaj/telekasten.lua
@@ -0,0 +1,52 @@
+lua << END
+local home = vim.fn.expand("~/Google Drive/2. - code/212. - vimwiki/212.01 - pkm")
+require('telekasten').setup({
+    home         = home,
+    take_over_my_home = true,
+    auto_set_filetype = true,
+    dailies      = home .. '/' .. 'daily',
+    weeklies     = home .. '/' .. 'weekly',
+    templates    = home .. '/' .. 'templates',
+    image_subdir = "img",
+    extension    = ".md",
+    prefix_title_by_uuid = false,
+    uuid_type = "%Y%m%d%H%M",
+    uuid_sep = "-",
+    follow_creates_nonexisting = true,
+    dailies_create_nonexisting = true,
+    weeklies_create_nonexisting = true,
+    journal_auto_open = false,
+    template_new_note = home .. '/' .. 'templates/new_note.md',
+    template_new_daily = home .. '/' .. 'templates/daily.md',
+    template_new_weekly= home .. '/' .. 'templates/weekly.md',
+    image_link_style = "markdown",
+    sort = "filename",
+    plug_into_calendar = true,
+    calendar_opts = {
+        weeknm = 4,
+        calendar_monday = 1,
+        calendar_mark = 'left-fit',
+    },
+    close_after_yanking = false,
+    insert_after_inserting = true,
+    tag_notation = "#tag",
+    command_palette_theme = "dropdown",
+    show_tags_theme = "ivy",
+    subdirs_in_links = true,
+    template_handling = "smart",
+    new_note_location = "smart",
+    rename_update_links = true,
+})
+END
+
+
+nnoremap <leader>zf :lua require('telekasten').find_notes()<CR>
+nnoremap <leader>zd :lua require('telekasten').find_daily_notes()<CR>
+nnoremap <leader>zb :lua require('telekasten').show_backlinks()<CR>
+nnoremap <leader>zg :lua require('telekasten').search_notes()<CR>
+nnoremap <leader>zz :lua require('telekasten').follow_link()<CR>
+nnoremap <leader>zn :lua require('telekasten').new_note()<CR>
+nnoremap <leader>zi :lua require('telekasten').insert_link()<CR>
+
+-- on hesitation, bring up the panel
+nnoremap <leader>Z :lua require('telekasten').panel()<CR>
---
+++ nvim/lua/abaj/vimwiki.lua
@@ -0,0 +1,44 @@
+vim.cmd([[
+let g:calendar_monday = 1
+let g:calendar_weeknm = 2 
+let g:vimwiki_list = [{'path': '$HOME/Google Drive/2. - code/212. - vimwiki/212.00 - cs',
+					  \ 'syntax': 'markdown', 'ext': '.md'},
+					  \ {'path': '$HOME/Google Drive/2. - code/212. - vimwiki/212.01 - pkm',
+					  \ 'syntax': 'markdown', 'ext': '.md'}]
+let g:vimwiki_global_ext = 0
+let g:vimwiki_folding = 'custom'
+let g:calendar_keys = { 'goto_today': 'T'}
+nmap <space><space> <Plug>VimwikiToggleListItem
+vmap <space><space> <Plug>VimwikiToggleListItem
+nmap <space>v <Plug>VimwikiIndex
+vmap <space>v <Plug>VimwikiIndex
+nmap <space>V <Plug>VimwikiDiaryIndex
+vmap <space>V <Plug>VimwikiDiaryIndex
+nmap <space>w <Plug>VimwikiMakeDiaryNote
+vmap <space>w <Plug>VimwikiMakeDiaryNote
+nmap <space>m <Plug>VimwikiMakeYesterdayDiaryNote
+vmap <space>m <Plug>VimwikiMakeYesterdayDiaryNote
+nmap <space>z <Plug>VimwikiMakeTomorrowDiaryNote
+vmap <space>z <Plug>VimwikiMakeTomorrowDiaryNote
+nmap <space>s :setlocal spell! spelllang=en_au<CR>
+vmap <space>s :setlocal spell! spelllang=en_au<CR>
+augroup folds
+	autocmd FileType vimwiki set foldexpr=MarkdownFold()
+	autocmd FileType vimwiki set foldmethod=expr
+	autocmd FileType vimwiki set syntax=markdown
+	autocmd FileType vimwiki set nofoldenable
+	autocmd FileType vimwiki set linebreak
+	autocmd FileType vimwiki set wrap
+    autocmd FileType vimwiki nmap <space>t <Plug>VimwikiDiaryNextDay
+    autocmd FileType vimwiki vmap <space>t <Plug>VimwikiDiaryNextDay
+    autocmd FileType vimwiki nmap <space>h <Plug>VimwikiDiaryPrevDay
+    autocmd FileType vimwiki vmap <space>h <Plug>VimwikiDiaryPrevDay
+    autocmd FileType vimwiki nmap <space>g <Plug>VimwikiDiaryGenerateLinks
+    autocmd FileType vimwiki vmap <space>g :<Plug>VimwikiDiaryGenerateLinks
+    autocmd FileType vimwiki nmap <space>c :CalendarVR<CR>
+    autocmd FileType vimwiki vmap <space>c :CalendarVR<CR>
+augroup END
+autocmd BufEnter diary.md VimwikiDiaryGenerateLinks
+autocmd BufEnter diary.md set syntax=vimwiki
+autocmd BufEnter index.md set syntax=vimwiki
+]])
