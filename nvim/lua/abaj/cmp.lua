5ad4856e4765d2e8d104ba5752f423ea801468ae
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-29 13:47:01 +1000

    cron 13:47:01

--- nvim/lua/abaj/cmp.lua
+++ nvim/lua/abaj/cmp.lua
@@ -131,15 +131,4 @@ cmp.setup.cmdline(
 -- Use friendly snippets
 require("luasnip.loaders.from_vscode").lazy_load()
 
--- Limit this beast
-local tabnine = require("cmp_tabnine.config")
-tabnine:setup(
-  {
-    max_lines = 1000,
-    max_num_results = 20,
-    sort = true,
-    run_on_every_keystroke = true,
-    snippet_placeholder = ".."
-  }
-)
 
