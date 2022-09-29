d9370b92dac5611697d24366953fcf17afff40c6
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-29 12:33:00 +1000

    cron 12:33:00

--- nvim/lua/abaj/lsp/mason.lua
+++ nvim/lua/abaj/lsp/mason.lua
@@ -59,19 +59,19 @@ local opts = {}
 
 for _, server in pairs(servers) do
   opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
+    on_attach = require("abaj.lsp.handlers").on_attach,
+    capabilities = require("abaj.lsp.handlers").capabilities,
   }
 
   server = vim.split(server, "@")[1]
 
   if server == "jsonls" then
-    local jsonls_opts = require "user.lsp.settings.jsonls"
+    local jsonls_opts = require "abaj.lsp.settings.jsonls"
     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
   end
 
   if server == "yamlls" then
-    local yamlls_opts = require "user.lsp.settings.yamlls"
+    local yamlls_opts = require "abaj.lsp.settings.yamlls"
     opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
   end
 
@@ -80,7 +80,7 @@ for _, server in pairs(servers) do
     if not l_status_ok then
       return
     end
-    -- local sumneko_opts = require "user.lsp.settings.sumneko_lua"
+    -- local sumneko_opts = require "abaj.lsp.settings.sumneko_lua"
     -- opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
     -- opts = vim.tbl_deep_extend("force", require("lua-dev").setup(), opts)
     local luadev = lua_dev.setup {
@@ -97,27 +97,27 @@ for _, server in pairs(servers) do
   end
 
   if server == "tsserver" then
-    local tsserver_opts = require "user.lsp.settings.tsserver"
+    local tsserver_opts = require "abaj.lsp.settings.tsserver"
     opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
   end
 
   if server == "pyright" then
-    local pyright_opts = require "user.lsp.settings.pyright"
+    local pyright_opts = require "abaj.lsp.settings.pyright"
     opts = vim.tbl_deep_extend("force", pyright_opts, opts)
   end
 
   if server == "solc" then
-    local solc_opts = require "user.lsp.settings.solc"
+    local solc_opts = require "abaj.lsp.settings.solc"
     opts = vim.tbl_deep_extend("force", solc_opts, opts)
   end
 
   if server == "emmet_ls" then
-    local emmet_ls_opts = require "user.lsp.settings.emmet_ls"
+    local emmet_ls_opts = require "abaj.lsp.settings.emmet_ls"
     opts = vim.tbl_deep_extend("force", emmet_ls_opts, opts)
   end
 
   if server == "zk" then
-    local zk_opts = require "user.lsp.settings.zk"
+    local zk_opts = require "abaj.lsp.settings.zk"
     opts = vim.tbl_deep_extend("force", zk_opts, opts)
   end
 
@@ -126,7 +126,7 @@ for _, server in pairs(servers) do
   end
 
   if server == "rust_analyzer" then
-    local rust_opts = require "user.lsp.settings.rust"
+    local rust_opts = require "abaj.lsp.settings.rust"
     -- opts = vim.tbl_deep_extend("force", rust_opts, opts)
     local rust_tools_status_ok, rust_tools = pcall(require, "rust-tools")
     if not rust_tools_status_ok then
