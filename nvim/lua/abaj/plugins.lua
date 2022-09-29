f9b860b2bffab3e6105d57e895922104e3d81ae4
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-29 13:45:03 +1000

    cron 13:45:03

--- nvim/lua/abaj/plugins.lua
+++ nvim/lua/abaj/plugins.lua
@@ -83,6 +83,7 @@ return packer.startup(function(use)
 	use "saadparwaiz1/cmp_luasnip"
 	use "hrsh7th/cmp-nvim-lsp"
     use "L3MON4D3/LuaSnip" --snippet engine
+    use "onsails/lspkind.nvim"
 	-- use "hrsh7th/cmp-nvim-lua"
     
 
