97bc116f94b38cc39fdad403796b14286f6378b7
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-29 11:46:01 +1000

    cron 11:46:01

--- nvim/lua/abaj/plugins.lua
+++ nvim/lua/abaj/plugins.lua
@@ -64,6 +64,16 @@ return packer.startup(function(use)
     use 'folke/tokyonight.nvim'
     use 'preservim/nerdtree'
 
+    -- cmp
+    use "hrsh7th/nvim-cmp"
+	use "hrsh7th/cmp-buffer"
+	use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-cmdline"
+	use "saadparwaiz1/cmp_luasnip"
+	-- use "hrsh7th/cmp-nvim-lsp"
+	-- use "hrsh7th/cmp-nvim-lua"
+    
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
