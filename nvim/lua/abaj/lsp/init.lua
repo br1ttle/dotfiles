e79a3232e9c480a7ac24f2a9110fdc6702fa1755
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-29 12:06:01 +1000

    cron 12:06:01

--- nvim/lua/abaj/lsp/init.lua
+++ nvim/lua/abaj/lsp/init.lua
@@ -3,7 +3,6 @@ if not status_ok then
   return
 end
 
-require("abaj.lsp.lsp-installer")
 require("abaj.lsp.handlers").setup()
 -- require "abaj.lsp.configs"
 -- require "abaj.lsp.null-ls"
--- nvim/lua/abaj/plugins.lua
+++ nvim/lua/abaj/plugins.lua
@@ -66,7 +66,6 @@ return packer.startup(function(use)
 
     -- lsp
     use 'neovim/nvim-lspconfig'
-	use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 	use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
     -- cmp
