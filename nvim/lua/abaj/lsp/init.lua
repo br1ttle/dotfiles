7bad6c778db8297f6b0ddb67914e7ff9b56023fd
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-29 12:09:00 +1000

    cron 12:09:00

--- nvim/lua/abaj/lsp/init.lua
+++ nvim/lua/abaj/lsp/init.lua
@@ -2,7 +2,3 @@ local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
   return
 end
-
-require("abaj.lsp.handlers").setup()
--- require "abaj.lsp.configs"
--- require "abaj.lsp.null-ls"
