6fdd60afe899410ac499268ceb66c6a8893c325a
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-18 09:38:01 +1100

    cron 09:38:01

--- nvim/lua/abaj/snippets.lua
+++ nvim/lua/abaj/snippets.lua
@@ -2,5 +2,7 @@ vim.cmd "let g:UltiSnipsExpandTrigger=\"<tab>\""
 vim.cmd "let g:UltiSnipsJumpForwardTrigger=\"<c-b>\""
 vim.cmd "let g:UltiSnipsJumpBackwardTrigger=\"<c-z>\""
 
+-- https://castel.dev/post/lecture-notes-1/
+
 -- " If you want :UltiSnipsEdit to split your window.
 -- let g:UltiSnipsEditSplit="vertical"
