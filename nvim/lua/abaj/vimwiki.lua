1b428f1030d60ffe1d48cd7b7b047e524927db42
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-11-24 02:49:01 +1100

    cron 02:49:01

--- nvim/lua/abaj/vimwiki.lua
+++ nvim/lua/abaj/vimwiki.lua
@@ -20,7 +20,7 @@ nmap <space>m <Plug>VimwikiMakeYesterdayDiaryNote
 vmap <space>m <Plug>VimwikiMakeYesterdayDiaryNote
 nmap <space>z <Plug>VimwikiMakeTomorrowDiaryNote
 vmap <space>z <Plug>VimwikiMakeTomorrowDiaryNote
-nmap <space>e <Plug>VimwikiTabnewLink
+nmap <space><E> <Plug>VimwikiTabnewLink
 nmap <space>s :setlocal spell! spelllang=en_au<CR>
 vmap <space>s :setlocal spell! spelllang=en_au<CR>
 augroup folds
