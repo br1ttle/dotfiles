d74227c1d0891015cfbe8759592b5af63644661d
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-12-13 16:43:00 +1100

    cron 16:43:00

--- nvim/lua/abaj/snippets.lua
+++ nvim/lua/abaj/snippets.lua
@@ -1,6 +1,6 @@
 vim.cmd "let g:UltiSnipsExpandTrigger=\"<Tab>\""
-vim.cmd "let g:UltiSnipsJumpForwardTrigger=\"<C-b>\""
-vim.cmd "let g:UltiSnipsJumpBackwardTrigger=\"<C-z>\""
+vim.cmd "let g:UltiSnipsJumpForwardTrigger=\"<C-u>\""
+vim.cmd "let g:UltiSnipsJumpBackwardTrigger=\"<C-U>\""
 
 -- https://castel.dev/post/lecture-notes-1/
 
