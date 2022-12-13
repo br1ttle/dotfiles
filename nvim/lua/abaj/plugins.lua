18b995e28db4961655d9e0eea2d8bcf4ca0fae24
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-12-13 17:43:01 +1100

    cron 17:43:00

--- nvim/lua/abaj/plugins.lua
+++ nvim/lua/abaj/plugins.lua
@@ -77,7 +77,44 @@ return packer.startup(function(use)
 
 
     -- cmp
-    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/nvim-cmp",
+    requires = {
+        "quangnguyen30192/cmp-nvim-ultisnips",
+        config = function()
+          -- optional call to setup (see customization section)
+          require("cmp_nvim_ultisnips").setup{}
+        end,
+        -- If you want to enable filetype detection based on treesitter:
+        -- requires = { "nvim-treesitter/nvim-treesitter" },
+        },
+        config = function()
+        local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
+        require("cmp").setup({
+          snippet = {
+            expand = function(args)
+              vim.fn["UltiSnips#Anon"](args.body)
+            end,
+          },
+          sources = {
+            { name = "ultisnips" },
+            -- more sources
+          },
+          -- recommended configuration for <Tab> people:
+          mapping = {
+            ["<Tab>"] = cmp.mapping(
+              function(fallback)
+                cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+              end,
+              { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+            ),
+            ["<S-Tab>"] = cmp.mapping(
+              function(fallback)
+                cmp_ultisnips_mappings.jump_backwards(fallback)
+              end,
+              { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+            ),
+          },
+        })
 	use "hrsh7th/cmp-buffer"
 	use "hrsh7th/cmp-path"
     use "hrsh7th/cmp-cmdline"
@@ -92,6 +129,8 @@ return packer.startup(function(use)
     -- snippets
     use "SirVer/ultisnips"
     use "honza/vim-snippets"
+
+
 
 
 	-- Automatically set up your configuration after cloning packer.nvim
